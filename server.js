fd5ba4104f996391fb12f892416f7fa2ea8f841b
gengstah <gerardpdelasarmas@gmail.com>
2014-07-23 21:33:15 +0800

    mapped prototype 2 to path /2

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ var server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname, 'public')));
 
+router.get('/2', function (req, res) {
+    res.sendfile('public/prototype2.html');
+});
+
 var port = Number(process.env.PORT || 5000);
 
 server.listen(port, function() {
